e017d94ba612285bae88b0111bab47760e03b86d
butlerem <butlerem01@gmail.com>
2024-09-18 19:56:55 -0400

    Add projects an feedbacks tables to database schema

--- db/schema.ts
+++ db/schema.ts
@@ -1,4 +1,5 @@
 import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
 
 export const projects = pgTable('projects', {
   id: serial("id").primaryKey(),
@@ -7,3 +8,24 @@ export const projects = pgTable('projects', {
   url: text("url"),
   userId: varchar("user_id"),
 });
+
+export const projectsRelations = relations
+(projects, ({ many }) => ({
+  feedbacks: many(feedbacks),
+}));
+
+export const feedbacks = pgTable("feedbacks", {
+  id: serial("id").primaryKey(),
+  projectId: varchar("project_id"),
+  feedback: text("feedback"),
+  userName: text("user_name"),
+  userEmail: text("user_email"),
+  message: text("message"),
+});
+
+export const feedbacksRelations = relations(feedbacks, ({ one}) => ({
+  project: one(projects, {
+    fields: [feedbacks.projectId],
+    references: [projects.id],
+  })
+}));
