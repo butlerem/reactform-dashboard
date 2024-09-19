1b5d0dbfce0846078775192d47df249d542f1efe
butlerem <butlerem01@gmail.com>
2024-09-19 02:30:44 -0400

    Implement customer creation and checkout session for subscriptions

--- app/(user)/dashboard/projects-list.tsx
+++ app/(user)/dashboard/projects-list.tsx
@@ -1,14 +1,18 @@
-import { InferSelectModal} from "drizzle-orm";
+import { InferSelectModel } from "drizzle-orm";
 import { projects } from "@/db/schema";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle, CardFooter } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-
+import SubscribeBtn from "../payments/subscribe-btn";
+import { monthlyPlanId } from "@/lib/payments";
+import { Lock } from "lucide-react";
+import { maxFreeProjects } from "@/lib/payments";
 
 type Project = InferSelectModel<typeof projects>;
 
 type Props = {
   projects: Project[];
+  subscribed: boolean | null | undefined;
 };
 
 const ProjectsList = (props: Props) => {
@@ -18,18 +22,30 @@ const ProjectsList = (props: Props) => {
         {props.projects.map((project: Project) => (
           <li key={project.id}>
             <Card className="max-w-[350px] flex flex-col h-full">
-            <CardHeader className="flex-1">
-            <CardTitle>{project.name}</CardTitle>
-            </CardHeader>
-            <CardFooter>
-              <Link href={`/projects/${project.id}`}>
-                <Button>View Project</Button>
-              </Link>
-            </CardFooter>
-
+              <CardHeader className="flex-1">
+                <CardTitle>{project.name}</CardTitle>
+                <CardDescription>{project.description}</CardDescription>
+              </CardHeader>
+              <CardFooter>
+                <Link href={`/projects/${project.id}`}>
+                  <Button>View Project</Button>
+                </Link>
+              </CardFooter>
             </Card>
           </li>
         ))}
+        {props.subscribed !== true && props.projects?.length > maxFreeProjects ?
+          <Card className="max-w-[350px] flex flex-col h-full bg-gray-300">
+            <CardHeader className="flex-1">
+              <CardTitle className="flex flex-row text-sm md:text-lg items-center">
+                <Lock className="h-4 w-4 md:h-8 md:w-8 mr-2" />
+                <span>Upgrade to Premium</span></CardTitle>
+              <CardDescription className="mt-3">Unlock unlimited projects</CardDescription>
+            </CardHeader>
+            <div className="w-fit mx-auto mb-4">
+              <SubscribeBtn price={monthlyPlanId} />
+            </div>
+          </Card> : null}
       </ul>
     </div>
   )
--- app/api/stripe/checkout-session/route.ts
+++ app/api/stripe/checkout-session/route.ts
@@ -32,4 +32,44 @@ export async function POST(req: Request) {
         dbId: userId,
       },
     };
+
+    const response = await stripe.customers.create(customerData);
+
+    customer = { id: response.id };
+
+    await db.insert(subscriptions).values({
+      userId,
+      stripeCustomerId: customer.id,
+    });
   }
+
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+
+  if (!customer?.id) {
+    return new Response(JSON.stringify({ error: "Failed to get a customer id" }), { status: 500 });
+  }
+
+  try {
+    const session = await stripe.checkout.sessions.create({
+      success_url: `${baseUrl}/payments/checkout-success`,
+      customer: customer?.id,
+      payment_method_types: ["card"],
+      mode: "subscription",
+      line_items: [
+        {
+          price,
+          quantity,
+        },
+      ],
+    });
+
+    if (session) {
+      return new Response(JSON.stringify({ sessionId: session.id }), { status: 200 });
+    } else {
+      return new Response(JSON.stringify({ error: "Failed to create a session" }), { status: 500 });
+    }
+  } catch (error) {
+    console.error(error);
+    return new Response(JSON.stringify({ error: "Failed to create a session" }), { status: 500 });
+  }
+}
