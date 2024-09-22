b1bdfdd05e32d501b34eac14ac2a7ca37fbbc44f
butlerem <butlerem01@gmail.com>
2024-09-22 19:03:09 -0400

    Display projects with subscription-based project creation limit

--- app/(user)/dashboard/page.tsx
+++ app/(user)/dashboard/page.tsx
@@ -1,9 +1,11 @@
-import NewProjBtn from "@/components/new-proj";
-import {db} from "@/db";
+import NewProjBtn from "@/components/new-proj"
+import { db } from "@/db";
 import { projects } from "@/db/schema";
 import { eq } from "drizzle-orm";
 import { auth } from "@clerk/nextjs/server";
 import ProjectsList from "./projects-list";
+import { getSubscription } from "@/actions/userSubscriptions";
+import { maxFreeProjects } from "@/lib/payments";
 
 export default async function Page() {
   const { userId } = auth();
@@ -13,12 +15,12 @@ export default async function Page() {
 
   const userProjects = await db.select().from(projects).where(eq(projects.userId, userId));
 
+  const subscribed = await getSubscription({ userId });
+
   return (
     <div>
-    <div className="flex items-center justify-center gap-3">
-      <h1 className="text-3xl font-bold text-center 
-      my-4">Your Projects</h1><NewProjBtn />
-    </div><ProjectsList projects=
-    {userProjects}/></div>
+      <div className="flex items-center justify-center gap-3">
+        <h1 className="text-3xl font-bold text-center my-4">Your Projects</h1>{subscribed !== true && userProjects.length > maxFreeProjects ? null : <NewProjBtn />}
+      </div><ProjectsList projects={userProjects} subscribed={subscribed} /></div>
   )
 }
--- lib/payments.ts
+++ lib/payments.ts
@@ -1,2 +1,3 @@
 export const monthlyPlanId: string = "price_1Q0dXCDtrBhLyuPng9XIsyyG";
 export const yearlyPlanId: string = "price_1Q0dZJDtrBhLyuPnTQxpHGYG";
+export const maxFreeProjects: number = 3;
