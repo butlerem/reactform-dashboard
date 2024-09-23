d27183be2af6258edeb96255a139f6b807df2f10
butlerem <butlerem01@gmail.com>
2024-09-22 22:14:16 -0400

    Update styling

--- app/landing-page/features-section.tsx
+++ app/landing-page/features-section.tsx
@@ -30,13 +30,11 @@ const features = [
 const FeaturesSection = () => {
   return (
     <section className="container mx-auto max-w-screen-xl px-4 my-24 flex items-center flex-col">
-      <h2 className="mb-6 text-2xl font-bold text-[var(--foreground)]">Features</h2>
+      <h2 className="mb-6 text-2xl font-bold">Features</h2>
       <div className="container mx-auto">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
           {features.map((feature, index) => (
-            <div key={index} className="bg-[var(--card-alt)] text-[var(--foreground)] p-5 border rounded-lg shadow-md">
-              <Feature title={feature.title} description={feature.description} />
-            </div>
+            <Feature key={index} {...feature} />
           ))}
         </div>
       </div>
